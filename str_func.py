8a1bfbd2e9b195d0141ac5198cd1013f90bd5833
Stanislav <132190234+BondarevichSI@users.noreply.github.com>
2023-05-07 20:19:48 +0900

    Update str_func.py
    addd commit

--- str_func.py
+++ str_func.py
@@ -2,4 +2,5 @@ def capital_letter(string):
     return string.upper()
 
 def capital_first(line):
+    """делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
     return line.title()
